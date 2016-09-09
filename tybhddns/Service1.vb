5d36c61dfe5f031e7764fc93062a56631f28309f
TuZi <TuZi@TUZI-BH-PC>
2016-09-09 12:24:31 +0800

    添加项目文件。

---
+++ tybhddns/Service1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Public Class Service1
+    Private sw As StreamWriter
+    Private sr As StreamReader
+    Public strread() As String
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' 请在此处添加代码以启动您的服务。此方法应完成设置工作，
+        ' 以使您的服务开始工作。
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' 在此处添加代码以执行任何必要的拆解操作，从而停止您的服务。
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        hosts_reader("C:\Windows\System32\drivers\etc\hosts")
+
+        Dim i As Integer = 0
+        Dim youtybh As Boolean = False
+        For i = 0 To strread.Length - 1
+            If strread（i） <> Nothing Then
+                If strread(i)(0) <> "#" Then
+
+                    If InStr(strread(i), "tybh.vicp.net") Then
+                        youtybh = True
+
+                        Try
+                            Dim dFile As New System.Net.WebClient
+                            Dim ip = "127.0.0.1"
+                            ip = dFile.DownloadString("http://code.taobao.org/svn/BHBnet/trunk/ip/ip.txt")
+                            strread(i) = Replace(ip, vbCrLf, "") & "     tybh.vicp.net"
+                        Catch ex As Exception
+
+                        End Try
+
+                    End If
+
+                End If
+
+            End If
+        Next
+
+        If youtybh = False Then
+            ReDim Preserve strread(strread.Length)
+            Try
+                Dim dFile As New System.Net.WebClient
+                Dim ip = "127.0.0.1"
+                ip = dFile.DownloadString("http://code.taobao.org/svn/BHBnet/trunk/ip/ip.txt")
+                strread(strread.Length - 1) = Replace(ip, vbCrLf, "") & "     tybh.vicp.net"
+            Catch ex As Exception
+
+            End Try
+
+        End If
+
+
+        hosts_writer("C:\Windows\System32\drivers\etc\hosts", strread)
+    End Sub
+
+    Public Sub hosts_writer(ByVal parth As String, ByVal str() As String)
+        Dim i As Integer
+        sw = New StreamWriter(parth, False, System.Text.Encoding.Default)
+        For i = 0 To str.Length - 1
+            sw.WriteLine(str(i))
+            sw.Flush()
+        Next
+        sw.Close()
+        sw = Nothing
+
+
+    End Sub
+
+    Public Function hosts_reader(ByVal parth As String)
+        Dim i As Integer = 0
+        Dim s As String
+        strread = Nothing
+
+        sr = New StreamReader(parth, System.Text.Encoding.Default)
+        Do While sr.Peek() <> -1
+            s = sr.ReadLine()
+            ReDim Preserve strread(i)
+
+            strread(i) = s
+            i = i + 1
+        Loop
+        sr.Close()
+        sr = Nothing
+
+        If i = 0 Then
+            ReDim Preserve strread(0)
+            strread(0) = "null"
+            Return strread
+
+        Else
+            Return strread
+        End If
+
+    End Function
+
+
+
+End Class
